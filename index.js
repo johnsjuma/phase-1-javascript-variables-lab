849b017087db736cd843be311045e37d35457124
jerryjohns <jerry.johns@student.moringaschool.com>
2022-06-08 14:34:15 +0300

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Code your solution in this file!
-let companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Chelsea'
-let companyCeo ='Susan Smith'
+const companyName = 'Scuber'
+let companyCeo = 'Susan Smith'
